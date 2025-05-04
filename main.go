fcb34543baab054a79df451873cf2fbbd69290cc
Folombas <folombas@yandex.ru>
2025-05-04 22:55:43 +0300

    Первый HTTP запрос

--- main.go
+++ main.go
@@ -1,29 +1,22 @@
 package main
 
 import (
+	"demo/weather/geo"
 	"flag"
 	"fmt"
-	"io"
-	"strings"
 )
 
 func main() {
-	fmt.Println("Старт разработки приложения Погоды на Go")
+	fmt.Println("Приложение Погоды на Go")
 	city := flag.String("city", "", "Город пользователя")
-	format := flag.Int("format", 1, "Формат вывода погоды")
+	// format := flag.Int("format", 1, "Формат вывода погоды")
 
 	flag.Parse()
 
 	fmt.Println(*city)
-	fmt.Println(*format)
-
-	r := strings.NewReader("Привет! Я поток данных")
-	block := make([]byte, 4)
-	for {
-		_, err := r.Read(block)
-		fmt.Printf("%q\n", block)
-		if err == io.EOF {
-			break
-		}
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
+	fmt.Println(geoData)
 }
